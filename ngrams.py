36b39846482b5bf4f44448b6934eb9b1530ce22d
stefanborasinski <stefanborasinski@gmail.com>
2020-05-14 21:27:32 +0000

    added word2vec+_+scratch+_+skip+_+251 to results.log

--- ngrams.py
+++ ngrams.py
@@ -39,6 +39,90 @@ class LanguageModel:
                         self._processline(line)
         except UnicodeDecodeError:
             print("UnicodeDecodeError processing {}: ignoring rest of file".format(afile))
+            
+    def _processline(self, line):
+        tokens = ["__START"] + tokenize(line) + ["__END"]
+        previous = "__END"
+        for token in tokens:
+            self.unigram[token] = self.unigram.get(token, 0) + 1
+            current = self.bigram.get(previous, {})
+            current[token] = current.get(token, 0) + 1
+            self.bigram[previous] = current
+            previous = token
+    
+    def _make_unknowns(self, known=2):
+        unknown = 0
+        for (k, v) in list(self.unigram.items()):
+            if v < known:
+                del self.unigram[k]
+                self.unigram["__UNK"] = self.unigram.get("__UNK", 0) + v
+        for (k, adict) in list(self.bigram.items()):
+            for (kk, v) in list(adict.items()):
+                isknown = self.unigram.get(kk, 0)
+                if isknown == 0:
+                    adict["__UNK"] = adict.get("__UNK", 0) + v
+                    del adict[kk]
+            isknown = self.unigram.get(k, 0)
+            if isknown == 0:
+                del self.bigram[k]
+                current = self.bigram.get("__UNK", {})
+                current.update(adict)
+                self.bigram["__UNK"] = current
+
+            else:
+                self.bigram[k] = adict
+                
+    def _discount(self, discount=0.75):
+        # discount each bigram count by a small fixed amount
+        self.bigram = {k: {kk: value - discount for (kk, value) in adict.items()} for (k, adict) in self.bigram.items()}
+
+        # for each word, store the total amount of the discount so that the total is the same
+        # i.e., so we are reserving this as probability mass
+        for k in self.bigram.keys():
+            lamb = len(self.bigram[k])
+            self.bigram[k]["__DISCOUNT"] = lamb * discount
+
+        # work out kneser-ney unigram probabilities
+        # count the number of contexts each word has been seen in
+        self.kn = {}
+        for (k, adict) in self.bigram.items():
+            for kk in adict.keys():
+                self.kn[kk] = self.kn.get(kk, 0) + 1
+                
+    def _convert_to_probs(self):
+
+        self.unigram = {k: v / sum(self.unigram.values()) for (k, v) in self.unigram.items()}
+        self.bigram = {key: {k: v / sum(adict.values()) for (k, v) in adict.items()} for (key, adict) in
+                       self.bigram.items()}
+        self.kn = {k: v / sum(self.kn.values()) for (k, v) in self.kn.items()}
+        
+    def compute_perplexity(self):
+
+        # compute the probability and length of the corpus
+        # calculate perplexity
+        # lower perplexity means that the model better explains the data
+
+        p, N = self.compute_probability()
+        # print(p,N)
+        pp = math.exp(-p / N)
+        return pp
+    
+    def compute_probability(self):
+        # computes the probability (and length) of a corpus contained in filenames
+        total_p = 0
+        total_N = 0
+        for i, afile in enumerate(self.files):
+            try:
+                with open(os.path.join(self.training_dir, afile)) as instream:
+                    for line in instream:
+                        line = line.rstrip()
+                        if len(line) > 0:
+                            p, N = self.compute_prob_line(line)
+                            total_p += p
+                            total_N += N
+            except UnicodeDecodeError:
+                print("UnicodeDecodeError processing file {}: ignoring rest of file".format(afile))
+        return total_p, total_N
 
     def test(self):
         acc = 0
@@ -69,22 +153,8 @@ class LanguageModel:
         log_results(args.mode + " " + self.__str__(), acc, len(scc.questions), correct,
                     incorrect, perplexity=self.compute_perplexity(), guessed=guessed)
 
-    def _processline(self, line):
-        tokens = ["__START"] + tokenize(line) + ["__END"]
-        previous = "__END"
-        for token in tokens:
-            self.unigram[token] = self.unigram.get(token, 0) + 1
-            current = self.bigram.get(previous, {})
-            current[token] = current.get(token, 0) + 1
-            self.bigram[previous] = current
-            previous = token
 
-    def _convert_to_probs(self):
 
-        self.unigram = {k: v / sum(self.unigram.values()) for (k, v) in self.unigram.items()}
-        self.bigram = {key: {k: v / sum(adict.values()) for (k, v) in adict.items()} for (key, adict) in
-                       self.bigram.items()}
-        self.kn = {k: v / sum(self.kn.values()) for (k, v) in self.kn.items()}
 
     def get_prob(self, token, context=""):
         if self.mp.get("method", "unigram") == "unigram":
@@ -112,73 +182,6 @@ class LanguageModel:
         for i, token in enumerate(tokens[1:]):
             acc += math.log(self.get_prob(token, tokens[:i + 1]))
         return acc, len(tokens[1:])
-
-    def _make_unknowns(self, known=2):
-        unknown = 0
-        for (k, v) in list(self.unigram.items()):
-            if v < known:
-                del self.unigram[k]
-                self.unigram["__UNK"] = self.unigram.get("__UNK", 0) + v
-        for (k, adict) in list(self.bigram.items()):
-            for (kk, v) in list(adict.items()):
-                isknown = self.unigram.get(kk, 0)
-                if isknown == 0:
-                    adict["__UNK"] = adict.get("__UNK", 0) + v
-                    del adict[kk]
-            isknown = self.unigram.get(k, 0)
-            if isknown == 0:
-                del self.bigram[k]
-                current = self.bigram.get("__UNK", {})
-                current.update(adict)
-                self.bigram["__UNK"] = current
-
-            else:
-                self.bigram[k] = adict
-
-    def _discount(self, discount=0.75):
-        # discount each bigram count by a small fixed amount
-        self.bigram = {k: {kk: value - discount for (kk, value) in adict.items()} for (k, adict) in self.bigram.items()}
-
-        # for each word, store the total amount of the discount so that the total is the same
-        # i.e., so we are reserving this as probability mass
-        for k in self.bigram.keys():
-            lamb = len(self.bigram[k])
-            self.bigram[k]["__DISCOUNT"] = lamb * discount
-
-        # work out kneser-ney unigram probabilities
-        # count the number of contexts each word has been seen in
-        self.kn = {}
-        for (k, adict) in self.bigram.items():
-            for kk in adict.keys():
-                self.kn[kk] = self.kn.get(kk, 0) + 1
-
-    def compute_perplexity(self):
-
-        # compute the probability and length of the corpus
-        # calculate perplexity
-        # lower perplexity means that the model better explains the data
-
-        p, N = self.compute_probability()
-        # print(p,N)
-        pp = math.exp(-p / N)
-        return pp
-
-    def compute_probability(self):
-        # computes the probability (and length) of a corpus contained in filenames
-        total_p = 0
-        total_N = 0
-        for i, afile in enumerate(self.files):
-            try:
-                with open(os.path.join(self.training_dir, afile)) as instream:
-                    for line in instream:
-                        line = line.rstrip()
-                        if len(line) > 0:
-                            p, N = self.compute_prob_line(line)
-                            total_p += p
-                            total_N += N
-            except UnicodeDecodeError:
-                print("UnicodeDecodeError processing file {}: ignoring rest of file".format(afile))
-        return total_p, total_N
 
 
 if __name__ == "__main__":
